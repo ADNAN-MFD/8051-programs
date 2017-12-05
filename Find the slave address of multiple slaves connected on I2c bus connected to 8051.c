cc09d018a41b2aa7a961fe15f88e8bc7e822ad58
Amal Poulose <amalpoulose1995@hotmail.com>
2017-12-06 01:57:33 +0530

    Add files via upload

---
+++ Find the slave address of multiple slaves connected on I2c bus connected to 8051.c
@@ -0,0 +1,35 @@
+/***** Find the slave address of multiple slaves connected on I2c bus connected to 8051*****/
+
+#include <reg51.h>
+#include <stdio.h>
+#include "delay_ms.h"
+#include "lcd8bit.h"
+#include "i2c.h"
+#include "i2c_device.h"
+
+void main()
+{
+	unsigned char i,temp,count=0;
+	lcd_init();
+	lcd_cmd(0x0c);
+	for(i=0;i<=254;i+=2)
+	{
+		lcd_cmd(0x01);
+		lcd_string("checking  : ");
+		lcd_hex(i);
+		temp=i2c_detect(i);
+		if(temp)
+		{
+			lcd_cmd(0xc0);
+			lcd_hex(i);
+			lcd_string("Found");
+			count++;
+		}
+		if(i>=254)
+			break;
+	}
+	lcd_cmd(0xc0);
+	lcd_string("Devices : ");
+	lcd_integer(count);
+  while(1);
+}
