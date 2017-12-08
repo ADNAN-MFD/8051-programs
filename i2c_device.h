1ef7184a98c90585978dadb309048adf8834e349
Amal Poulose <amalpoulose1995@hotmail.com>
2017-12-09 01:42:40 +0530

    Update i2c_device.h

--- i2c_device.h
+++ i2c_device.h
@@ -1,4 +1,3 @@
-
 void i2c_device_write(unsigned char addr, unsigned char mry ,unsigned char _data)
 {
 	i2c_start();
@@ -40,4 +39,44 @@ unsigned char i2c_detect(unsigned char addr)
 	i2c_stop();
 	delay_ms(10);
 	return flag;
+}
+//multiple memory write frame
+void i2c_device_mwrite(unsigned char addr, unsigned char mry ,unsigned char *_data,unsigned char n)
+{
+	unsigned char i;
+	i2c_start();
+	i2c_write(addr);
+	i2c_ack();
+	i2c_write(mry);
+	i2c_ack();
+	for(i=0;i<n;i++)
+	{
+	i2c_write(_data[i]);
+	i2c_ack();
+	}
+	i2c_stop();
+	delay_ms(10);
+}
+
+//multiple memory read frame based on datasheet of ds1307
+void i2c_device_mread(unsigned char addr,unsigned char mry,unsigned char *_data,unsigned char n)
+{
+	char i=0;
+	i2c_start();
+	i2c_write(addr);
+	i2c_ack();
+	i2c_write(mry);
+	i2c_ack();
+	i2c_start();
+	i2c_write(addr|1);
+	i2c_ack();
+	_data[i]=i2c_read();
+	for(i=1;i<n;i++)
+	{
+		i2c_dack();
+	_data[i]=i2c_read();
+	}
+	i2c_nack();
+	i2c_stop();
+	delay_ms(10);
 }
